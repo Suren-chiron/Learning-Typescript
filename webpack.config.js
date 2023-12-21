9686ccc3e3de28c13da21f3daaed4231c8557f71
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2023-12-21 13:02:03 +0330

    adding "entry" for base file and devtool, module : for some rules and exclude folder, resolve: for changing all ts file to js file

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
+// info : i configured this webpack config for creating bundle of Module folder
+
 const path = require('path');
 
-module.export = {
-   entry: './src/index.ts',
+module.exports = {
+   entry: './Module/result.ts',
    output: {
       filename: 'bundle.js',
       path: path.resolve(__dirname, 'dist'),
    },
+   devtool: 'inline-source-map',
+   module: {
+      rules: [{ test: /\.ts$/, use: 'ts-loader', exclude: '/node_modules/' }],
+   },
+   resolve: { extensions: ['.ts', '.js'] },
 };
