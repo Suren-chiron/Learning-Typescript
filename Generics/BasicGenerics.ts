cfa69351f1a122906f4fe42ee9fd4755ae7fc40d
Mohammad Karimi <mohammadkarimi.swctw@gmail.com>
2024-01-07 19:34:41 +0330

    Add more example for BasicGeneric

--- Generics/BasicGenerics.ts
+++ Generics/BasicGenerics.ts
@@ -1,6 +1,27 @@
 const nameGe: Array<string> = [];
 nameGe[0].split('');
 
+///
+
+type TLiteralColor = 'green' | 'blue' | 'black';
+const WhichColor: Array<TLiteralColor> = ['black', 'blue'];
+
+///
+
+type TDataStorageGeneric<T> = {
+   storage: T[];
+   add: (data: T) => void;
+};
+
+const textStorageGeneric: TDataStorageGeneric<string> = {
+   storage: ['test', 'test2'],
+   add(data: 'test') {
+      console.log(data);
+   },
+};
+
+///
+
 const promise: Promise<number> = new Promise((resolve, reject) => {
    setTimeout(() => {
       resolve(12);
